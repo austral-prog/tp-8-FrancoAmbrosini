bf3dfea55adc62e776108129826b7a08a8587ce7
FrancoAmbrosini <162723096+FrancoAmbrosini@users.noreply.github.com>
2024-05-24 21:40:04 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,31 +1,20 @@
 from sets_categories_data import (ALCOHOLS)
 
 
-def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
+def clean_ingredients(plato, ingredientes):
+    ingredientes_clean = set(ingredientes)
 
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
-
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
-
-    return ()
+    return (plato,ingredientes_clean)
 
 
-def check_drinks(drink_name, drink_ingredients):
-    """Append "Cocktail" (alcohol)  or "Mocktail" (no alcohol) to `drink_name`, based on `drink_ingredients`.
 
-    :param drink_name: str - name of the drink.
-    :param drink_ingredients: list - ingredients in the drink.
-    :return: str - drink_name appended with "Mocktail" or "Cocktail".
-
-    The function should return the name of the drink followed by "Mocktail" (non-alcoholic) and drink
-    name followed by "Cocktail" (includes alcohol).
-
-    """
-
-    return ""
-
+def check_drinks(bebida, ingredientes):
+    contiene_alcohol= False
+    for ingrediente in ingredientes:
+        if ingrediente in ALCOHOLS:
+            contiene_alcohol = True
+            break
+    if contiene_alcohol:
+        return f"{bebida} Cocktail"
+    else:
+        return f"{bebida} Mocktail"
